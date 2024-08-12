90fabe41b7ec2dcc211df837a5c321bbc1d5b13c
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-08-12 10:37:16 -0500

    Now we generate random positions when starting the map

--- Map.py
+++ Map.py
@@ -5,14 +5,26 @@ class Tron_Map:
     def __init__(self, width=6, height=6, num_players=2):
         self._width = width
         self._height = height
+
+        if (num_players > width * height / 2):
+            print("Invalid number of players for the size")
+            raise()  # TODO
+
         self._num_players = num_players
         self._map = [[0 for x in range(width)] for y in range(height)]
 
         self._current_player = 1
-        self._positions = [[random.randint(0, width - 1), random.randint(0, height - 1)] for _ in range(num_players)]
+        self._positions = self._generate_random_positions()
+        
+        for i, pos in enumerate(self._positions):
+            self._map[pos[0]][pos[1]] = i + 1
 
     def get_map(self):
         return self._map
+
+    def print_map(self):
+        for row in self._map:
+            print(row)
 
     def add_move(self, x, y):
         self._map[y][x] = self._current_player
@@ -28,3 +40,15 @@ class Tron_Map:
     def _next_player(self):
         self._current_player = (self._current_player % self._num_players) + 1
 
+    def _generate_random_positions(self):
+        positions = []
+
+        for _ in range(self._num_players):
+            rand_pos = (random.randint(0, self._width - 1), random.randint(0, self._height - 1))
+
+            while self._map[rand_pos[0]][rand_pos[1]] != 0:
+                rand_pos = (random.randint(0, self._width), random.randint(0, self._height))
+
+            positions.append(rand_pos)  # Return the current random position (honestly used yield just because)
+
+        return positions
---
+++ test.py
@@ -0,0 +1,9 @@
+from Map import Tron_Map
+
+
+def main():
+    map = Tron_Map()
+    map.print_map()
+
+if __name__ == '__main__':
+    main()
