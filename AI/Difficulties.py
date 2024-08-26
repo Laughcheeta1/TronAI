9bd8cac4ec36b20c85303af8178b8f5c7b0f1c60
Simón González Montoya <99984830+SimonGonMon@users.noreply.github.com>
2024-08-26 11:32:07 -0500

    fix: remove redundant parenthesis haha

--- AI/Difficulties.py
+++ AI/Difficulties.py
@@ -1,28 +1,29 @@
-from . import TronTree
 from . import DifficultyConstants
+from . import TronTree
+
 
 def recommend(game, maxPlayer, difficulty) -> list[int, int]:
-  """
-  Recommends a move for the player based on the difficulty level.
+    """
+    Recommends a move for the player based on the difficulty level.
 
-  Parameters:
-  - game: The game object to be used for the tree.
-  - maxPlayer: Boolean indicating if the player is Max (True) or Min (False).
-  - difficulty: The difficulty level of the AI.
+    Parameters:
+    - game: The game object to be used for the tree.
+    - maxPlayer: Boolean indicating if the player is Max (True) or Min (False).
+    - difficulty: The difficulty level of the AI.
 
-  Returns: The recommended move.
-  """
-  # Get the recommended move depending on the difficulty level
-  if(str.lower(difficulty) == "hard"):
-    tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.HARD[1])
-    move = tree.alphaBeta(DifficultyConstants.HARD[0], maxPlayer)
-  elif(str.lower(difficulty) == "medium"):
-    tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.MEDIUM[1])
-    move = tree.alphaBeta(DifficultyConstants.MEDIUM[0], maxPlayer)
-  else:
-    if(str.lower(difficulty) != "easy"):
-      print("Invalid difficulty level. Using easy level by default.")
-    tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.EASY[1])
-    move = tree.alphaBeta(DifficultyConstants.EASY[0], maxPlayer)
+    Returns: The recommended move.
+    """
+    # Get the recommended move depending on the difficulty level
+    if str.lower(difficulty) == "hard":
+        tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.HARD[1])
+        move = tree.alphaBeta(DifficultyConstants.HARD[0], maxPlayer)
+    elif str.lower(difficulty) == "medium":
+        tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.MEDIUM[1])
+        move = tree.alphaBeta(DifficultyConstants.MEDIUM[0], maxPlayer)
+    else:
+        if str.lower(difficulty) != "easy":
+            print("Invalid difficulty level. Using easy level by default.")
+        tree = TronTree(game, maxPlayer, nodeReachableSpaces=DifficultyConstants.EASY[1])
+        move = tree.alphaBeta(DifficultyConstants.EASY[0], maxPlayer)
 
-  return move.state[0] if maxPlayer else move.state[1]
+    return move.state[0] if maxPlayer else move.state[1]
