c72ac8aa080c7dcf618858564552a304413b8058
Simón González Montoya <99984830+SimonGonMon@users.noreply.github.com>
2024-08-27 17:56:09 -0500

    refactor: test other constants and maximum render distance

--- AI/DifficultyConstants.py
+++ AI/DifficultyConstants.py
@@ -2,6 +2,6 @@
 This constants are used to define the difficulty of the game, the higher the difficulty, the more steps the AI will take to find the solution.
 """
 
-EASY = (3, 3)
-MEDIUM = (5, 5)
-HARD = (7, 7)
+EASY = (3, 10)
+MEDIUM = (4, 20)
+HARD = (6, 40)
